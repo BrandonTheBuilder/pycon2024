7c3cbe3e4fa76ef6a59b7a0031010762b67dc899
Loren Arthur <loren.sarthur@gmail.com>
2024-05-07 12:23:59 -0700

    Update step for benchmark

--- playground/run_bench.py
+++ playground/run_bench.py
@@ -3,6 +3,8 @@ import subprocess
 TARGETS_PATH = "examples/too_many_extensions/BUCK"
 
 N = 8000
+START = 0
+STEP = 50
 
 def update_targets(n):
     args = [
@@ -20,12 +22,12 @@ def run_buck(n):
         "examples/too_many_extensions:import_test",
         "--",
         f"{n}",
-        "import_stats.csv",
+        "short_import_stats.csv",
     ]
     subprocess.run(args)
     
 def main():
-    for n in range(1, N + 1):
+    for n in range(START, N + 1, STEP):
         update_targets(n)
         run_buck(n)
 
